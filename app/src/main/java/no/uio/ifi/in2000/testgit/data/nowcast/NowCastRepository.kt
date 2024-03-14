40665c8cd2d735499bf28f4b55f13084a06f1440
Bjørnar Holm-Marøy <bjoholmm@uio.no>
2024-03-14 15:28:50 +0100

    Update NowCastRepository.kt

--- app/src/main/java/no/uio/ifi/in2000/testgit/data/nowcast/NowCastRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/testgit/data/nowcast/NowCastRepository.kt
@@ -1,10 +1,12 @@
-package no.uio.ifi.in2000.testgit.data.nowcast
+package no.uio.ifi.in2000.testgit.data.NowCast
 
 import com.example.example.Timeseries
 
 
-class NowCastRepository(private val nowCastDataSource: NowCastDataSource = NowCastDataSource()) {
-    suspend fun fetchNowCast(lat: String, lon: String): Timeseries? {
-        return nowCastDataSource.getData(lat, lon)
+class NowCastRepository(
+    private val nowCastDataSource: NowCastDataSource = NowCastDataSource()
+) {
+    suspend fun fetchNowCast(): Timeseries? {
+        return nowCastDataSource.getData()
     }
 }
