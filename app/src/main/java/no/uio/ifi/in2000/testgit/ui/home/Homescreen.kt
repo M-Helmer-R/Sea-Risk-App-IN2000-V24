1d4b2939e4c045a74da2c2da0c4fa0689e0d7106
Bjørnar Holm-Marøy <bjoholmm@uio.no>
2024-03-19 15:35:39 +0100

    Map in homescreen that shows norway

--- app/src/main/java/no/uio/ifi/in2000/testgit/ui/home/Homescreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/testgit/ui/home/Homescreen.kt
@@ -17,12 +17,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapboxExperimental
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
 import no.uio.ifi.in2000.testgit.model.CityDatabase.CityDatabase
 import no.uio.ifi.in2000.testgit.ui.Activity.ActivityScreen
 import no.uio.ifi.in2000.testgit.ui.Activity.ActivityScreenViewModel
 
+@OptIn(MapboxExperimental::class)
 @Composable
 fun Homescreen(navController: NavController, homescreenViewModel: HomescreenViewModel = viewModel()){
 
+    MapboxMap(
+        Modifier.fillMaxSize(),
+        mapViewportState = MapViewportState().apply {
+            setCameraOptions {
+                zoom(3.7)
+                center(Point.fromLngLat(11.49537, 64.01487 ))
+                pitch(0.0)
+                bearing(0.0)
+            }
+        },
+    )
 
 }
