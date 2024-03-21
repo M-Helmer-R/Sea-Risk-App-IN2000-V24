301290e0e08666982bc275941b5ba96e020a3bae
kastubda <kastubda@uio.no>
2024-03-21 11:17:42 +0100

    Added oceanforecast datafiles and datasource, does not work properly yet

---
+++ app/src/main/java/no/uio/ifi/in2000/testgit/data/oceanforecast/OceanForeCastDataSource.kt
@@ -0,0 +1,40 @@
+package no.uio.ifi.in2000.testgit.data.oceanforecast
+
+
+
+import com.example.example.NowcastData
+import com.example.example.Timeseries
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.serialization.gson.gson
+import no.uio.ifi.in2000.testgit.model.oceanforecast.OceanProperties
+import no.uio.ifi.in2000.testgit.model.oceanforecast.OceanTimeseries
+
+
+class OceanForeCastDataSource {
+    val client = HttpClient() {
+        install(ContentNegotiation) {
+            gson()
+        }
+        defaultRequest {
+            url("https://gw-uio.intark.uh-it.no/in2000/")
+
+            header("X-Gravitee-API-Key", "71f2d9f6-8fbf-480e-b098-e905dcb29a51")
+        }
+
+    }
+
+    suspend fun getData(): OceanTimeseries {
+        val oceanforecastOslo = "weatherapi/oceanforecast/2.0/complete?lat=59.9139&lon=10.7522"
+        val kallNowcastOslo = client.get(oceanforecastOslo)
+        val dataNowcastOslo = kallNowcastOslo.body<OceanProperties>()
+        val instantNowcastData = dataNowcastOslo.timeseries[0]
+
+
+        return instantNowcastData
+    }
+}
--- app/src/main/java/no/uio/ifi/in2000/testgit/test.kt
+++ app/src/main/java/no/uio/ifi/in2000/testgit/test.kt
@@ -4,13 +4,14 @@ package no.uio.ifi.in2000.testgit
 
 import no.uio.ifi.in2000.testgit.data.metalerts.MetAlertsDataSource
 import no.uio.ifi.in2000.testgit.data.nowcast.*
+import no.uio.ifi.in2000.testgit.data.oceanforecast.OceanForeCastDataSource
 
 
 suspend fun main() {
     val lat = "59.9"
     val lon = "10.7"
-    val nowCastDataSource = NowCastDataSource()
-    val nowcastData = nowCastDataSource.getData(lat, lon)
+    val nowCastDataSource = OceanForeCastDataSource()
+    val nowcastData = nowCastDataSource.getData()
     println(nowcastData)
     //val metAlertsDataSource: MetAlertsDataSource = MetAlertsDataSource()
     //val metAlertsData = metAlertsDataSource.getMetAlerts()
