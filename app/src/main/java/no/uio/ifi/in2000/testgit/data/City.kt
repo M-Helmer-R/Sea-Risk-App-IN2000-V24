b75a11d113824c9ca364936f0ea75e01d2ab4931
snfiger <snfiguer@uio.no>
2024-04-03 13:45:17 +0200

    -

--- app/src/main/java/no/uio/ifi/in2000/testgit/data/City.kt
+++ app/src/main/java/no/uio/ifi/in2000/testgit/data/City.kt
@@ -3,10 +3,22 @@ package no.uio.ifi.in2000.testgit.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Query
 
-@Entity
+@Entity(tableName = "cities")
 data class City (
     @PrimaryKey val cityId: Int,
+    @ColumnInfo(name = "name") val name : String,
     @ColumnInfo(name = "longitude") val lon : Double,
     @ColumnInfo(name = "lattitude") val lat : Double,
      )
+
+interface CityDao {
+    @Query("SELECT * FROM cities WHERE name == :name")
+    fun findByName(name : String) : City
+
+    @Query("SELECT * FROM cities WHERE cityId == :id")
+    fun findByName(id : Int) : City
+}
+
+
