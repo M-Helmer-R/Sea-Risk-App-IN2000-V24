4a7430f0610041534a0ff5ce973c95be60e69ce7
kastubda <kastubda@uio.no>
2024-04-17 10:55:35 +0200

    Added datasource for reverse geocoding

---
+++ app/src/main/java/no/uio/ifi/in2000/testgit/data/map/GeoCodeDataSource.kt
@@ -0,0 +1,49 @@
+package no.uio.ifi.in2000.testgit.data.map
+
+import android.util.Log
+import com.mapbox.api.geocoding.v5.GeocodingCriteria
+import com.mapbox.api.geocoding.v5.MapboxGeocoding
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse
+import com.mapbox.geojson.Point
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+interface ReverseGeocodeCallback {
+    fun onSuccess(placeName: String)
+    fun onFailure(placeName: String)
+}
+class GeoCodeDataSource {
+    fun reverseGeoCode(lon: Double, lat: Double, callback: ReverseGeocodeCallback) {
+        var placeNameDisplay = "Test"
+        val reverseGeocode = MapboxGeocoding.builder()
+            .accessToken("sk.eyJ1IjoiYmpvaG9sbW0iLCJhIjoiY2x0eWVwZHp5MGRmaTJrcGpueG8zcTR1MCJ9.zal9bJ3fdxMij0MJB-GvUQ")
+            .query(Point.fromLngLat(lon, lat))
+            .geocodingTypes(GeocodingCriteria.TYPE_ADDRESS)
+            .build()
+
+
+        reverseGeocode.enqueueCall(object : Callback<GeocodingResponse> {
+            override fun onResponse(call: Call<GeocodingResponse>, response: Response<GeocodingResponse>) {
+                val results = response.body()?.features()
+                if (results != null && results.size > 0) {
+                    val firstResult = results[0]
+                    val placeName = firstResult.placeName()
+                    if (placeName != null) {
+                        callback.onSuccess(placeName)
+                    }
+                    //isDialogVisible = true
+                    Log.i("Geocoding", "Place name: $placeName")
+                } else {
+                    callback.onFailure("Ingen Data")
+                    Log.i("Geocoding", "No results found.")
+                }
+            }
+
+            override fun onFailure(call: Call<GeocodingResponse>, throwable: Throwable) {
+                Log.e("Geocoding", "Error: " + throwable.message)
+            }
+
+        })
+    }
+}
--- app/src/main/java/no/uio/ifi/in2000/testgit/ui/map/MapScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/testgit/ui/map/MapScreen.kt
@@ -21,21 +21,23 @@ import com.mapbox.geojson.Point
 import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
+import no.uio.ifi.in2000.testgit.data.map.ReverseGeocodeCallback
 
 import com.mapbox.api.geocoding.v5.GeocodingCriteria
 import com.mapbox.api.geocoding.v5.MapboxGeocoding
 import com.mapbox.api.geocoding.v5.models.GeocodingResponse
+import no.uio.ifi.in2000.testgit.data.map.GeoCodeDataSource
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Call
 
 @OptIn(MapboxExperimental::class)
 @Composable
-fun Mapscreen(){
+fun Mapscreen(geoCodeDataSource: GeoCodeDataSource = GeoCodeDataSource()){
     var isDialogVisible by remember { mutableStateOf(false) }
     var lat: Double
     var lon: Double
-    var placeNameDisplay by remember { mutableStateOf("Error") }
+    var placeNameDisplay by remember { mutableStateOf("Ingen Data") }
 
     Box {
         if (isDialogVisible){
@@ -64,40 +66,17 @@ fun Mapscreen(){
                 lat = point.latitude()
                 lon = point.longitude()
 
-                val reverseGeocode = MapboxGeocoding.builder()
-                    .accessToken("sk.eyJ1IjoiYmpvaG9sbW0iLCJhIjoiY2x0eWVwZHp5MGRmaTJrcGpueG8zcTR1MCJ9.zal9bJ3fdxMij0MJB-GvUQ")
-                    .query(Point.fromLngLat(lon, lat))
-                    .geocodingTypes(GeocodingCriteria.TYPE_ADDRESS)
-                    .build()
-
-
-                reverseGeocode.enqueueCall(object : Callback<GeocodingResponse> {
-                    override fun onResponse(call: Call<GeocodingResponse>, response: Response<GeocodingResponse>) {
-                        val results = response.body()?.features()
-                        if (results != null && results.size > 0) {
-                            val firstResult = results[0]
-                            val placeName = firstResult.placeName()
-                            if (placeName != null) {
-                                placeNameDisplay = placeName
-                            }
-                            isDialogVisible = true
-                            Log.i("Geocoding", "Place name: $placeName")
-                        } else {
-                            Log.i("Geocoding", "No results found.")
-                        }
+                geoCodeDataSource.reverseGeoCode(lon, lat, object: ReverseGeocodeCallback{
+                    override fun onSuccess(placeName: String){
+                        placeNameDisplay = placeName
                     }
 
-                    override fun onFailure(call: Call<GeocodingResponse>, throwable: Throwable) {
-                        Log.e("Geocoding", "Error: " + throwable.message)
+                    override fun onFailure(placeName: String){
+                        placeNameDisplay = placeName
                     }
                 })
 
-                println(reverseGeocode.toString()+ "Testgeocode" )
-                //isDialogVisible = true
-
-
-
-
+                isDialogVisible = true
 
                 true
             },
