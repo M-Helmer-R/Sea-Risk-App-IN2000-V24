048aa693ba51da2e11bb961bf8861a25e78e068c
Bjørnar Holm-Marøy <bjoholmm@uio.no>
2024-04-17 13:00:14 +0200

    updata topbar

--- app/src/main/java/no/uio/ifi/in2000/testgit/ui/home/Homescreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/testgit/ui/home/Homescreen.kt
@@ -1,4 +1,3 @@
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,11 +12,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Place
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
@@ -27,26 +24,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
-import no.uio.ifi.in2000.testgit.R
+import no.uio.ifi.in2000.testgit.ui.map.TopBar
 
 data class CityInfo(
     val name: String,
@@ -70,27 +54,7 @@ val White = Color(0xFFFFFFFF)
 fun Homescreen(navController: NavController?, currentRoute: String) {
 
     Column(modifier = Modifier.fillMaxSize()) {
-        TopAppBar(
-            title = {
-                Text(text = "Plask", color = Color.White, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
-            },
-            navigationIcon = {
-                Image(
-                    painter = painterResource(id = R.drawable.ikon),
-                    contentDescription = "Tilpasset Ikon",
-                    modifier = Modifier
-                        .size(50.dp)
-                        .clip(CircleShape)
-                )
-            },
-            actions = {
-                IconButton(onClick = { /* Komme oss inn på søk i kartskjerm */ }) {
-                    Icon(Icons.Filled.Search, contentDescription = "Søk", modifier = Modifier.size(50.dp), tint= White)
-                }
-            },
-            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = DarkBlue),
-            modifier = Modifier.zIndex(1f)
-        )
+        TopBar()
         LazyColumn(
             modifier = Modifier
                 .weight(1f)
