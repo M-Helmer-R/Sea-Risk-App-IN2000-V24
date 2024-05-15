03025863dd7d8cb40a1b87844a37fbddd2c0f348
snfiger <snfiguer@uio.no>
2024-05-15 15:13:51 +0200

    Heyo

--- app/src/main/java/no/uio/ifi/in2000/testgit/ui/home/dialog/LocationDialog.kt
+++ app/src/main/java/no/uio/ifi/in2000/testgit/ui/home/dialog/LocationDialog.kt
@@ -13,9 +13,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import no.uio.ifi.in2000.testgit.ui.home.HomeEvent
 
 @RequiresPermission(
